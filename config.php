808126e208cb106210ab5c2e34b786dba587327c
ahmdhafizh <ahmad.hafizh@ralali.com>
2020-05-09 12:11:25 +0700

    setup database

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'password');
+define('DB_NAME', 'disburse_db');
+define('BASIC_AUTH_USERNAME', 'HyzioY7LP6ZoO7nTYKbG8O4ISkyWnX1JvAEVAhtWKZumooCzqp41');
